f0e0ab33eea68c12dae148b2a00af370920e6122
Golam Mostofa <mostofart@gmail.com>
2024-10-31 18:51:25 +0600

    updated tutorial

---
+++ Assets/FirstScript.cs
@@ -0,0 +1,32 @@
+using System;
+using UnityEngine;
+
+public class FirstScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnEnable()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void FixedUpdate()
+    {
+        throw new NotImplementedException();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

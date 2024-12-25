9037f96e0fc478d0ffd5f69437453b466215212f
Golam Mostofa <mostofart@gmail.com>
2024-12-25 20:02:53 +0600

    tutorial update

--- Assets/_triggers/TiggerTest.cs
+++ Assets/_triggers/TiggerTest.cs
@@ -1,6 +1,20 @@
+using System;
 using UnityEngine;
 
 public class TiggerTest : MonoBehaviour
 {
-   
+    private void OnTriggerEnter(Collider other)
+    {
+       Debug.Log($"OnTriggerEnter: {other.name}");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log($"OnTriggerStay: {other.name}");
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log($"OnTriggerExit: {other.name}");
+    }
 }

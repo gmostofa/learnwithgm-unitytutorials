42006f798e1f434d7692d67ffa2cb2358186fdb2
Golam Mostofa <mostofart@gmail.com>
2025-02-22 20:31:03 +0600

    character controller tutorial added.

---
+++ Assets/character_controller/CharacterMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpHeight = 2f;
+    public float gravity = -9.81f;
+    public CharacterController controller;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // Ground check
+        isGrounded = controller.isGrounded;
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Movement input
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = transform.right * moveX + transform.forward * moveZ;
+        controller.Move(move * speed * Time.deltaTime);
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // Apply gravity
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}

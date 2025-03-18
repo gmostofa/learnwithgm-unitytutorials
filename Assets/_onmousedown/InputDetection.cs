6c039f5100b50bf8405dd2bf4c647a170bcd9220
Golam Mostofa <mostofart@gmail.com>
2025-03-18 17:28:14 +0600

    raycast tutorial added.

--- Assets/_onmousedown/InputDetection.cs
+++ Assets/_onmousedown/InputDetection.cs
@@ -3,11 +3,21 @@ using UnityEngine;
 
 public class InputDetection : MonoBehaviour
 {
+   public LayerMask layerMask;
    private void Update()
    {
-       if (Input.GetKey(KeyCode.A))
-       {
-           Debug.Log("A");
-       }
+      if (Input.GetMouseButtonDown(0))
+      {
+         DetectObject();
+      }
+   }
+
+   private void DetectObject()
+   {
+      Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+      if (Physics.Raycast(ray, out RaycastHit hitInfo, 1000, layerMask))
+      {
+         Debug.Log(hitInfo.collider.name);
+      }
    }
 }

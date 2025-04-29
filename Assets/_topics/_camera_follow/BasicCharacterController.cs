836dc3af91d803c4fe2ed38070ad5527be32846f
Golam Mostofa <mostofart@gmail.com>
2025-04-30 00:04:49 +0600

    Jump code added.

--- Assets/_topics/_camera_follow/BasicCharacterController.cs
+++ Assets/_topics/_camera_follow/BasicCharacterController.cs
@@ -3,21 +3,37 @@ using UnityEngine;
 
 public class BasicCharacterController : MonoBehaviour
 {
-  [SerializeField] private CharacterController characterController;
-  public float moveSpeed = 5f;
-
-
-  private void Start()
-  {
-    characterController = GetComponent<CharacterController>();
-  }
-
-  private void Update()
-  {
-    float horizontal = Input.GetAxis("Horizontal");
-    float vertical = Input.GetAxis("Vertical");
+    [SerializeField] private CharacterController characterController;
+    public float moveSpeed = 5f;
     
-    Vector3 moveDirection = transform.forward * vertical + transform.right * horizontal;
-    characterController.Move(moveDirection * (moveSpeed * Time.deltaTime));
-  }
+    public Vector3 velocity;
+    public float gravity = -9.81f;
+    public float jumpHeight = 2f;
+
+    private void Start()
+    {
+      characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+      float horizontal = Input.GetAxis("Horizontal");
+      float vertical = Input.GetAxis("Vertical");
+      
+      Vector3 moveDirection = transform.forward * vertical + transform.right * horizontal;
+      characterController.Move(moveDirection * (moveSpeed * Time.deltaTime));
+
+      if (characterController.isGrounded && velocity.y < 0)
+      {
+          velocity.y = -2f;
+      }
+      
+      velocity.y += gravity * Time.deltaTime;
+      characterController.Move(velocity * Time.deltaTime);
+
+      if (Input.GetButtonDown("Jump") && characterController.isGrounded)
+      {
+          velocity.y = Mathf.Sqrt(2 * gravity * jumpHeight * -1);
+      } 
+    }
 }

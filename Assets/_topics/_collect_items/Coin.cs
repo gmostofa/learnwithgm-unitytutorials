79b1045851ae09ed486f3088b03747c3d69f4854
Golam Mostofa <mostofart@gmail.com>
2025-05-03 14:46:47 +0600

    collectible object added.

--- Assets/_topics/_collect_items/Coin.cs
+++ Assets/_topics/_collect_items/Coin.cs
@@ -1,9 +1,33 @@
 using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
    public int coinValue;
+
+   public float flotDistance = 0.5f;
+   public float floatDuration = 0.5f;
+   public float rotateSpeed = 90f;
+   public float pulseScale = 1.1f;
+   public float pulseDuration = 0.5f;
+   
+
+   private void Start()
+   {
+       transform.DOMoveY(transform.position.y + flotDistance, floatDuration)
+           .SetLoops(-1, LoopType.Yoyo)
+           .SetEase(Ease.InOutFlash);
+       
+       transform.DORotate(new Vector3(0, 360,0), 2f, RotateMode.FastBeyond360)
+           .SetLoops(-1)
+           .SetEase(Ease.Linear);
+
+       transform.DOScale(pulseScale, pulseDuration)
+           .SetLoops(-1, LoopType.Yoyo)
+           .SetEase(Ease.InOutBounce);
+   }
+
 
    private void OnTriggerEnter(Collider other)
    {

e2e6f7849bd08cc9c7193bb3a8d3476484de163d
Golam Mostofa <mostofart@gmail.com>
2026-03-06 23:10:44 +0600

    unity save system playerprefs

--- Assets/_topics/_unity_saving/PlayerPrefsExample.cs
+++ Assets/_topics/_unity_saving/PlayerPrefsExample.cs
@@ -1,7 +1,32 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class PlayerPrefsExample : MonoBehaviour
 {
-   
+   public Button saveButton;
+   public TMP_InputField valueInput;
+   public TextMeshProUGUI valueText;
+
+
+   private void Start()
+   {
+      LoadData();
+      saveButton.onClick.AddListener(SaveData);
+   }
+
+
+   public void SaveData()
+   {
+      PlayerPrefs.SetString("anything_you_want_as_a_key", valueInput.text);
+      PlayerPrefs.Save();
+      Debug.Log($"You saved {valueInput.text}");
+   }
+
+   public void LoadData()
+   {
+      string loadValue = PlayerPrefs.GetString("anything_you_want_as_a_key");
+      valueText.text = loadValue;
+   }
 }

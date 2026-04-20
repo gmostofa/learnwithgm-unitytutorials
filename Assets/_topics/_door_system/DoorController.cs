df29b429f9526eb9e59c09c45e6d82cbbd344c25
Golam Mostofa <mostofart@gmail.com>
2026-04-20 09:02:23 +0600

    door system work

---
+++ Assets/_topics/_door_system/DoorController.cs
@@ -0,0 +1,49 @@
+using System;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    public float openAngle = 90;
+    public float speed = 2f;
+    
+    private Quaternion closedRotation;
+    private Quaternion openRotation;
+    
+    private bool isOpen = false;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        closedRotation = transform.rotation;
+        openRotation = Quaternion.Euler(0, openAngle, 0) * closedRotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen)
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, openRotation, Time.deltaTime * speed);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Lerp(transform.rotation, closedRotation, Time.deltaTime * speed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            isOpen = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        { 
+            isOpen = false;
+        }
+    }
+}
